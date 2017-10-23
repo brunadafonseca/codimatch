38041c9decfccd1e6dbc3349f5a8b88d3e8446d8
brunadafonseca <brunadafonseca@gmail.com>
2017-10-23 12:09:23 +0200

    Check if user is admin

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
   def home
+    @user = current_user
+    if @user.is_admin?
+      @users = User.all
+    else
+      flash[:notice] = "You're not an admin"
+    end
   end
 end
