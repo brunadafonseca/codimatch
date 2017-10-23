bd6841d60d6b0d3fb0682d83fbc2078face92690
FrisoDev <frisodevstoker@gmail.com>
2017-10-23 16:05:05 +0200

    succesfully de- and promotes users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,16 @@ class UsersController < ApplicationController
 
   def index
     @users = User.all
+        @user = User.find(current_user[:id])
   end
 
   def show
     @user = User.find(current_user[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+      @user.toggle!(:admin)
+    redirect_to users_path
   end
 end
