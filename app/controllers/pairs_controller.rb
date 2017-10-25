6cd10438443c4184d5a2a9bb93b7df24f5f9198d
FrisoDev <frisodevstoker@gmail.com>
2017-10-25 14:53:26 +0200

    broekn display

--- app/controllers/pairs_controller.rb
+++ app/controllers/pairs_controller.rb
@@ -2,6 +2,6 @@ class PairsController < ApplicationController
   def index
     @users = User.all
     @user = User.find(current_user[:id])
-    @randomshit = ['a', 'b', 'c', 'd']
+    @pair = Pair.new
   end
 end
