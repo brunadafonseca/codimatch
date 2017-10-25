38136555bda8d568aa58621bd32723726daed770
brunadafonseca <brunadafonseca@gmail.com>
2017-10-25 17:32:30 +0200

    Admins can see matches

--- app/models/pair.rb
+++ app/models/pair.rb
@@ -1,24 +1,27 @@
 class Pair < ApplicationRecord
-  has_and_belongs_to_many :users, dependent: :destroy
+  has_and_belongs_to_many :users
   belongs_to :day
 
-  def pair_students(students = [])
-    students.shuffle!
+  def self.pair_students
+    students = User.all.students.pluck(:email)
     number_of_pairs = (students.size) / 2
     number_of_days = (students.size) - 1
     a = students.first(number_of_pairs)
     b = students.last(number_of_pairs)
 
+    @all_pairs = []
     number_of_days.times do
-      all_pairs = []
+
       i = 0
       while i < number_of_pairs
-        all_pairs.push([a[i], b[i]])
+        @all_pairs.push([a[i], b[i]])
         i += 1
       end
 
+      puts "#{@all_pairs}"
       a.insert(1, b.shift)
       b.insert(-1, a.pop);
     end
+    return @all_pairs
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
-  has_and_belongs_to_many :pairs, dependent: :destroy
+  has_and_belongs_to_many :pairs
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
    def is_admin?
      self.admin?
    end
+
+   scope :students, -> {where(admin: false)}
 end
