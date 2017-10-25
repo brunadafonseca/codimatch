043d22fa89b860bdb9e9db913ca8faa01b27d94a
FrisoDev <frisodevstoker@gmail.com>
2017-10-25 11:14:04 +0200

    Pair model&con created

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
+  has_many :pairs
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
@@ -8,4 +9,7 @@ class User < ApplicationRecord
      self.admin?
    end
 
+   def is_student?
+     !self.admin?
+   end
 end
---
+++ spec/controllers/pairs_controller_controller_spec.rb
@@ -0,0 +1,5 @@
+require 'rails_helper'
+
+RSpec.describe PairsControllerController, type: :controller do
+
+end
