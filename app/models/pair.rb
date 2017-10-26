9a4512a6c767e1fd5032059811b983cd71959c9f
FrisoDev <frisodevstoker@gmail.com>
2017-10-26 11:37:31 +0200

    fixed displaying users in pairs

--- app/models/pair.rb
+++ app/models/pair.rb
@@ -4,11 +4,12 @@ class Pair < ApplicationRecord
   def self.generate_pairs
     @users = User.all
     @students = @users.students.pluck(:email)
+    @students.shuffle!
 
     number_of_pairs = (@students.size) / 2
     number_of_days = (@students.size) - 1
-    a = students.first(number_of_pairs)
-    b = students.last(number_of_pairs)
+    a = @students.first(number_of_pairs)
+    b = @students.last(number_of_pairs)
 
     @all_pairs = []
     number_of_days.times do
