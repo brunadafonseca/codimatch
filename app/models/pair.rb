8408c77ccd51b0b97ceac299abdfb8874f0424de
brunadafonseca <brunadafonseca@gmail.com>
2017-10-26 11:04:26 +0200

    Update pairs controller

--- app/models/pair.rb
+++ app/models/pair.rb
@@ -2,9 +2,11 @@ class Pair < ApplicationRecord
   has_many :users
 
   def self.generate_pairs
-    students = User.all.students.pluck(:email)
-    number_of_pairs = (students.size) / 2
-    number_of_days = (students.size) - 1
+    @users = User.all
+    @students = @users.students.pluck(:email)
+
+    number_of_pairs = (@students.size) / 2
+    number_of_days = (@students.size) - 1
     a = students.first(number_of_pairs)
     b = students.last(number_of_pairs)
 
