625c3393633e5ce1881d0eb232e42fa6b2b95b47
FrisoDev <frisodevstoker@gmail.com>
2017-10-27 12:58:20 +0200

    more shit

--- spec/feature/switch_admin_spec.rb
+++ spec/feature/switch_admin_spec.rb
@@ -1,12 +1,10 @@
-# feature "Pro- and Demoting" do
-#   scenario "Promote a pleb" do
-#
-#     visit create_user_path
-#     fill_in 'user_field', with: "blabla@blabla.com"
-#     fill_in 'password_field', with: "blabla"
-#     fill_in 'password_confirmation', with: "blabla"
-#     find("Sign Up").click
-#     expect(page).to have_content('Remember')
-#   end
-#
-# end
+feature "Pro- and Demoting" do
+      let!(:user) { create(:user) }
+  scenario "Promote a pleb" do
+
+    visit new_user_registration_path
+
+    expect(page).to have_content("Log In")
+  end
+
+end
